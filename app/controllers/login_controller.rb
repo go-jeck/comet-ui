0a33c7011f6a24f4c04a00d6c8fe14b43aad7e0e
reeechart <ferdinandusrichard@yahoo.co.id>
2018-07-25 11:54:56 +0700

    [andri/reeechart] add plain, simple login page

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,19 @@
+class LoginController < ApplicationController
+  require 'httparty'
+  require 'json'
+
+  def index
+    puts "Hiiiii"
+  end
+
+  def log_in
+    puts "Hello"
+    @response = HTTParty.get('http://localhost:8000/ping')
+    json = JSON.parse(@response.body)
+    if (json["success"] == "pong")
+      redirect_to login_index_path
+    else
+      redirect_to login_index_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'login/index'
+  # get 'log_in', to: 'login#log_in', as: 'log_in'
+  get 'ping' => 'login#log_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
