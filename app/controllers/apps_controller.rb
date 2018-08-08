e1e7000c486be6341339261c4abed88825ed8060
wajdifarid <faridwajdi05@gmail.com>
2018-08-08 19:19:18 +0700

    add namespaces page

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -4,13 +4,24 @@ class AppsController < ApplicationController
   before_action :login
   def index
     response = HTTParty.get("http://localhost:8000/application", :headers => {"Authorization" => cookies[:token]})
+    
     json_response = JSON.parse(response.body)
     @applications = Array.new
     for application in json_response do
-        app = App.new(application["application_name"], application["namespace"][0], 0)
-        @applications.push(app)
+      app = App.new(application["application_name"], application["namespace"][0], 0)
+      @applications.push(app)
     end
   end
-
+  def show
+    response = HTTParty.get("http://localhost:8000/application", :headers => {"Authorization" => cookies[:token]})
+    json_response = JSON.parse(response.body)
     
+    @app = nil 
+    for app in json_response do
+      if app["application_name"] == params[:name]
+        @app = {"application_name" => app["application_name"], "namespaces" => app["namespace"]}
+        break
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'configuration' => 'configuration#index'
   get 'configuration/edit'
   get 'apps' => 'apps#index'
+  get '/apps/:name', to: 'apps#show', as: 'app'
   get 'login' => 'login#index'
   get '/logout' => 'login#log_out'
   post '/login' => 'login#log_in'
   post '/commit' => 'configuration#commit_configurations'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
