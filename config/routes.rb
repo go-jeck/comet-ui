8beb8c31e99412ece0da5f466d5039760d200082
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-10 12:24:14 +0700

    change root into application index

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get '/logout' => 'login#log_out'
   post '/login' => 'login#log_in'
   post '/commit' => 'configuration#commit_configurations'
+
+  root 'apps#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
